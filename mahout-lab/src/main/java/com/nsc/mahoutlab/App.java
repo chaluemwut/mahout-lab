6cfbd545a7db99b99ea1a8b4c20e3de09914f04e
chaluemwut_noyunsan <chaluemwut@gmail.com>
2015-05-11 19:39:31 +0700

    Init project

---
+++ mahout-lab/src/main/java/com/nsc/mahoutlab/App.java
@@ -0,0 +1,13 @@
+package com.nsc.mahoutlab;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
